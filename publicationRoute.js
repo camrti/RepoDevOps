ba45e7877c3125b802ae2e685d329a7d8da28a37
lessandro00 <a.ambrosone8@studenti.unisa.it>
2024-05-14 11:21:42 +0200

    rwename

--- publicationRoute.js
+++ publicationRoute.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 router.use(express.json());
-const researcherLib = require('./publicationLib');
+const publicationLib = require('./publicationLib');
 
 router
     .get('/researcher=:name', async (req, res)=>{
         try{
             let researcher = req.params.name;
             const [name, lastName] = researcher.split('_');
-            let data = await researcherLib.getScopusData(name, lastName);
+            let data = await publicationLib.getScopusData(name, lastName);
             res.send(data);
         }catch (err){
             console.log(err);
@@ -18,7 +18,7 @@ router
     .get('/researcherId=:id', async (req, res)=>{
         try{
             let researcherId = req.params.id;
-            let data = await researcherLib.getPublication(researcherId);
+            let data = await publicationLib.getPublication(researcherId);
             res.send(data);
         }catch (err){
             console.log(err);
