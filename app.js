aed53ad08619128fefdd078cf83052410ce8f804
camilla <c.murati@studenti.unisa.it>
2024-05-14 12:29:46 +0200

    added researcherRoute

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const researcherRoute = require('./researcherRoute');
+const publicationRoute = require('./publicationRoute');
 
 app.get('/',(req, res) => {
     res.send('Hello World!');
 })
 
 app.use("/scopus",require(`./publicationRoute`));
+app.use("/researcher", researcherRoute);
 
 app.listen(port, ()=> {
     console.log(`Server listening on port ${port}!`);
