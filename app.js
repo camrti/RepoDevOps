01a854368c0955e2de0c9ead0f20a8db15acc974
camilla <c.murati@studenti.unisa.it>
2024-05-18 17:13:48 +0200

    app.js modificato

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const getCinecaData = require('./researcher/researcher.js'); // Modifica 
+il percorso per importare il modulo researchers
+
 const app = express();
-const port = 8000;
-const researcherRoute = require('./researcher/researcherRoute');
-const publicationRoute = require('./publication/publicationRoute');
+const PORT = process.env.PORT || 3000;
 
-app.get('/',(req, res) => {
-    res.send('Hello World!');
-})
+app.set('view engine', 'ejs');
 
-app.use("/scopus", publicationRoute);
-app.use("/researcher", researcherRoute);
+// Middleware per il parsing del corpo della richiesta
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(port, ()=> {
-    console.log(`Server listening on port ${port}!`);
-})
+// Serve i file statici dalla directory 'public'
+app.use(express.static('public'));
+
+// Pagina iniziale
+app.get('/', (req, res) => {
+  res.render('index'); // Renderizza index
+});
+
+// Route per la ricerca dei ricercatori
+app.post('/search', async (req, res) => {
+  const { researcherName } = req.body;
+
+  try {
+    // Ottieni i dati dei ricercatori
+    const researchers = await getCinecaData(researcherName);
+
+    res.render('search', { researchers });
+  } catch (error) {
+    console.error('Errore:', error);
+    res.render('error');
+  }
+});
+
+
+
+// Avvia il server
+app.listen(PORT, () => {
+  console.log(`Server avviato sulla porta ${PORT}`);
+});
+
