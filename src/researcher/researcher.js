5838da978f78c53e53ff126574a38e243af11615
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-19 17:26:01 +0200

    Fix in research.js: Cineca data sanification

--- src/researcher/researcher.js
+++ src/researcher/researcher.js
@@ -1,6 +1,19 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+function cleanString(value, isName=false, isSurname=false ) {
+    str = value.text()
+
+    if (isSurname)
+        str = str.split(" ")[0];
+    if (isName)
+        str = str.split(" ")[1];
+
+    str = str.replace(/\s+/g, ' ').trim().toLowerCase();
+
+    return str;
+}
+
 async function getCinecaData(name) {
     const url = 
 `https://cercauniversita.mur.gov.it/php5/docenti/vis_docenti.php?docinput=${encodeURIComponent(name)}&docsubmit=cerca`;
@@ -16,31 +29,35 @@ async function getCinecaData(name) {
         const table = $('table.risultati');
 
         // Extract data from the table
-        const data = [];
+        let data = {};
+
         table.find('tr').each((index, row) => {
             // Ignore the table header
             if ($(row).find('th').length === 0) {
                 const cells = $(row).find('td');
-                data.push({
-                    fascia: cells.eq(0).text().trim(),
-                    cognome_nome: cells.eq(1).text().trim(),
-                    genere: cells.eq(2).text().trim(),
-                    ateneo: cells.eq(3).text().trim(),
-                    facolta: cells.eq(4).text().trim(),
-                    ssd: cells.eq(5).text().trim(),
-                    sc: cells.eq(6).text().trim(),
-                    struttura: cells.eq(7).text().trim(),
-                    servizio_altro_ateneo: cells.eq(8).text().trim()
-                });
+                data ={
+                    fascia: cleanString(cells.eq(0)),
+                    cognome: cleanString(cells.eq(1),isSurname=true),
+                    nome: cleanString(cells.eq(1), isName=true),
+                    genere: cleanString(cells.eq(2)),
+                    ateneo: cleanString(cells.eq(3)),
+                    facolta: cleanString(cells.eq(4)),
+                    ssd: cleanString(cells.eq(5)),
+                    sc: cleanString(cells.eq(6)),
+                    struttura: cleanString(cells.eq(7)),
+                    servizio_altro_ateneo: cleanString(cells.eq(8))
+                };
             }
         });
         console.log('Data parsed from Cineca by Researcher');
         return data;
     } catch (error) {
         console.error('Error:', error);
-        return [];
+        return {};
     }
 }
 module.exports = {
     getCinecaData
 }; 
+
+getCinecaData("Francesco_Moscato")
--- src/researcher/researcher.test.js
+++ src/researcher/researcher.test.js
@@ -3,15 +3,17 @@ const researcher = require('./researcher.js');
 
 describe('Testing getCinecaData function', () => {
     let testCases = [
-        { name: 'Francesco_Moscato', expectedResult: [{ "ateneo":"SALERNO",
-                                                        "cognome_nome":"MOSCATO Francesco",
-                                                        "facolta":"","fascia":"Associato",
-                                                        "genere":"M",
-                                                        "sc":"09/H1",
+        { name: 'Francesco_Moscato', expectedResult: { "ateneo":"salerno",
+                                                        "cognome":"moscato",
+                                                        "nome": "francesco",
+                                                        "facolta":"",
+                                                        "fascia":"associato",
+                                                        "genere":"m",
+                                                        "sc":"09/h1",
                                                         "servizio_altro_ateneo":"",
-                                                        "ssd":"ING-INF/05",
-                                                        "struttura":"Ingegneria dell\'Informazione ed Elettrica e Matematica applicata"}]},
-        { name: 'Pasquale_Caggiano', expectedResult: []}
+                                                        "ssd":"ing-inf/05",
+                                                        "struttura":"ingegneria dell'informazione ed elettrica e matematica applicata"}},
+        { name: 'Pasquale_Caggiano', expectedResult: {}}
     ];
 
     it('should have test cases loaded', () => {
@@ -20,18 +22,19 @@ describe('Testing getCinecaData function', () => {
 
     it(`should return the Francesco Moscato information`, async () => {
         const data = await researcher.getCinecaData(testCases[0].name);
+        console.log(data);
         expect(data.ateneo).toEqual(testCases[0].expectedResult.ateneo);
         expect(data.cognome_nome).toEqual(testCases[0].expectedResult.cognome_nome);
         expect(data.facolta).toEqual(testCases[0].expectedResult.facolta);
         expect(data.genere).toEqual(testCases[0].expectedResult.genere);
         expect(data.sc).toEqual(testCases[0].expectedResult.sc);
-        expect(data.servizio_altro_ateneo).toEqual(testCases[0].expectedResult.atservizio_altro_ateneoeneo);
+        expect(data.servizio_altro_ateneo).toEqual(testCases[0].expectedResult.servizio_altro_ateneo);
         expect(data.ssd).toEqual(testCases[0].expectedResult.ssd);
         expect(data.struttura).toEqual(testCases[0].expectedResult.struttura);
 
     });
 
-    it(`should return an empty array`, async () => {
+    it(`should return an empty object`, async () => {
         const data = await researcher.getCinecaData(testCases[1].name);
         expect(data).toEqual(testCases[1].expectedResult);
     });
