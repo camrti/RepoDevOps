5512bfb7d79b4aa75897cb6a99aebb24e827ddd2
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-23 17:22:13 +0200

    Create a test Case JSON file, add researcherRoute user input sanitization

--- src/researcher/researcherRoute.js
+++ src/researcher/researcherRoute.js
@@ -2,26 +2,31 @@ require('dotenv').config();
 const express = require('express');
 const researcher = require('./researcher.js');
 const router = express.Router();
+const {query} = require('express-validator');
+
 
 // Route to search researchers
-router.get('/search', async (req, res) => {
+router.get('/search',[
+  query('researcherName').escape()
+], async (req, res) => {
+  
   const { researcherName } = req.query;
+  console.log('ResearcherRoute: ', researcherName);
   try {
     // Obtain researchers data
     const researchers = await researcher.getCinecaData(researcherName);
 
     if (researchers.length === 0)
-      res.status(404); 
-    else 
+      res.status(404).send('No researcher found');
+    else {
       res.status(200);
-    
-    res.json(researchers);    
-    console.log('Data from getCinecaData by ResearcherRoute');
-    return;
+      res.json(researchers);    
+      console.log('Data from getCinecaData by ResearcherRoute');
+    } 
+  
   } catch (error) {
       console.error('Error:', error);
-      res.status(500);
-      return;
+      res.status(500).send('Server error');
   }
 });
 
