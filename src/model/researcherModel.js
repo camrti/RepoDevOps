91ddaeb4ac55d3b8d3a08ab2eef0bef5a50dee55
lessandro00 <a.ambrosone8@studenti.unisa.it>
2024-05-23 17:27:44 +0200

    modifiche researcherModel, aggiunto docker.compose per db

--- src/model/researcherModel.js
+++ src/model/researcherModel.js
@@ -4,44 +4,38 @@ const db = require('../database/connection').main;
 const researcherSchema = new mongoose.Schema({
     fascia: {
         type: String,
-        required: true
     },
     cognome : {
         type: String,
-        required: true
     },
     nome: {
         type: String,
-        required: true
     },
     genere: {
         type: String,
-        required: true
     },
     ateneo: {
         type: String,
-        required: true
     },
     facolta: {
         type: String,
-        required: true
     },
     ssd: {
         type: String,
-        required: true
     },
     sc: {
         type: String,
-        required: true
     },
     struttura: {
         type: String,
-        required: true
     },
     servizio_altro_ateneo: {
         type: String,
-        required: true
-    }
+    },
+    pubblicazioni: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pubblication',
+    }]
 });
 
 const Researcher = mongoose.model('Researcher', researcherSchema);
