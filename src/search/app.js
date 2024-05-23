999e0d6a5499fcb51e6909bccc34572548dcefe9
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-23 15:11:47 +0200

    Fix .env read

--- src/search/app.js
+++ src/search/app.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: require('find-config')('.env') })
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
