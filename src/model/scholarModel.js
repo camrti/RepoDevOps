3b5f394edf509b04326c1febabf5c575b961b08d
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-25 13:18:12 +0200

    DB design modification, fix Cineca and Scholar data retrival from DB and API, (SCOPUS NOT DONE)

---
+++ src/model/scholarModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const db = require('../database/connection').main;
+
+const scholarSchema = new mongoose.Schema({
+    hIndex: {
+        type: String
+    },
+    citations: {
+        type: String
+    },
+    publications: [
+        {
+            title: {
+                type: String
+            },
+            authors: {
+                type: String
+            },
+            paperType: {
+                type: String
+            },
+            year: {
+                type: String
+            },
+            citationCount: {
+                type: Number
+            },
+            publicationLink: {
+                type: String
+            }
+        }
+    ]
+});
+
+
+const Scholar = mongoose.model('Scholar', scholarSchema);
+module.exports = Scholar
--- src/search/search.js
+++ src/search/search.js
@@ -1,10 +1,11 @@
 // the client app must interact only with the search module, see the architecture diagram
 const axios = require('axios');
-const Researcher = require('../model/researcherModel');
-const Publication = require('../model/publicationModel');
+const Cineca = require('../model/cinecaModel');
+const Scholar = require('../model/scholarModel');
+const Scopus = require('../model/scopusModel'); 
 
 // Function to get the data from ReseracherRoute
-async function getResearchers(researcherName) {
+async function getCinecaInfo(researcherName) {
   try {
     // Get the data from Cineca
     const response = await axios.get(`http://localhost:8001/search?researcherName=${encodeURIComponent(researcherName)}`);
@@ -18,9 +19,9 @@ async function getResearchers(researcherName) {
 
 
 // Function to get the data from PublicationRoute
-async function getPublications(researcherAteneo, researcherSurname, researcherName) {
+async function getScholarInfo(researcherAteneo, researcherSurname, researcherName) {
   try {
-    // Get the data from Cineca
+    // Get the data from Scholar
     const response = await axios.get(`http://localhost:8002/parse?ateneo=${encodeURIComponent(researcherAteneo)}&surname=${encodeURIComponent(researcherSurname)}&name=${encodeURIComponent(researcherName)}`);
     console.log('Data retrieved from Publication Service by Search');
     return response.data;
@@ -34,7 +35,7 @@ async function getPublications(researcherAteneo, researcherSurname, researcherNa
 // Function to get the data from ScopusRoute
 async function getScopusInfo(researcherAteneo, researcherSurname, researcherName) {
   try {
-    // Get the data from Cineca
+    // Get the data from Scopus
     const response = await axios.get(`http://localhost:8003/scopus?ateneo=${encodeURIComponent(researcherAteneo)}&surname=${encodeURIComponent(researcherSurname)}&name=${encodeURIComponent(researcherName)}`);
     console.log('Data retrieved from Scopus Service by Search');
     return response.data;
@@ -44,17 +45,33 @@ async function getScopusInfo(researcherAteneo, researcherSurname, researcherName
   }
 }
 
-
-
 // Function to get the resarcher data from DB
-async function getResearcherFromDB(researcherName) {
-  const researchers = await Researcher.find({
+async function getByNameCinecaInfoFromDB(researcherName) {
+  const cinecaInfo = await Cineca.find({
     $or: [
         { firstName: new RegExp(researcherName, 'i') },
         { lastName: new RegExp(researcherName, 'i') }
       ]
     });
-    return researchers;
+    return cinecaInfo;
+}
+
+// Function to get the resarcher data from DB
+async function getByIDCinecaInfoFromDB(cinecaID) {
+  const info = await Cineca.findById(cinecaID);
+    return info;
+}
+
+// Function to get the resarcher data from DB
+async function getByIDScholarInfoFromDB(scholarID) {
+  const scholarInfo = await Scholar.findById(scholarID);
+    return scholarInfo;
+}
+
+// Function to get the resarcher data from DB
+async function getByIDScopusInfoFromDB(scopusID) {
+  const scopusInfo = await Scopus.findById(scopusID);
+    return scopusInfo;
 }
 
 // // Function to get the publication data from Database
@@ -64,12 +81,12 @@ async function getResearcherFromDB(researcherName) {
 // }
 
 // Function to write the publication data from Database
-async function writeResearcherToDB(researchers) {
+async function writeCinecaInfoToDB(cinecaInfo) {
   let results = [];
 
-  for (const researcher of researchers) {
+  for (const info of cinecaInfo) {
     try {
-      let res = await Researcher.create(researcher);
+      let res = await Cineca.create(info);
       results.push(res);
     } catch (err) {
       console.log("Failed researcher to write to DB", err);
@@ -79,17 +96,28 @@ async function writeResearcherToDB(researchers) {
   return results;
 }
 
-// // DA FINIRE
-// // Function to write the publication data from Database
-// async function writePublicationToDB(researcherName){
-
-// }
-
+// Function to write the publication data from Database
+async function writeScholarInfoToDB(cinecaID, scholarInfo) {
+  scholarInfo = await Scholar.create(scholarInfo);
+  const cinecaInfo = await Cineca.findByIdAndUpdate(cinecaID, { scholarID: scholarInfo._id }, { new: true });
+  return scholarInfo;
+}
+// Function to write the publication data from Database
+async function writeScopusInfoToDB(cinecaID, scopusInfo) {
+    scopusInfo = await Scopus.create(scopusInfo);
+    const cinecaInfo = await Cineca.findByIdAndUpdate(cinecaID, { scopusID: scopusInfo._id }, { new: true });
+    return scopusInfo;
+}
 
 module.exports = {
-  getResearchers,
-  getPublications,
+  getCinecaInfo,
+  getScholarInfo,
   getScopusInfo,
-  getResearcherFromDB,
-  writeResearcherToDB
+  getByNameCinecaInfoFromDB,
+  getByIDCinecaInfoFromDB,
+  writeCinecaInfoToDB,
+  getByIDScholarInfoFromDB,
+  getByIDScopusInfoFromDB,
+  writeScopusInfoToDB,
+  writeScholarInfoToDB
 }; 
