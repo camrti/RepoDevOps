04aeb953be791b13fb6260b0025994e47f61438b
vinzcamp8 <vinzclash01@gmail.com>
2024-05-25 18:56:08 +0200

    Update script

--- packer/script.sh
+++ packer/script.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)
+if [ "$(docker ps -q)" ]; then
+    docker stop $(docker ps -a -q)
+fi
 
-docker rmi -f $(docker images -a | grep 'my' | awk '{print $3}')
+if [ "$(docker ps -a -q)" ]; then
+    docker rm $(docker ps -a -q)
+fi
+
+if [ "$(docker images -a | grep 'my' | awk '{print $3}')" ]; then
+    docker rmi -f $(docker images -a | grep 'my' | awk '{print $3}')
+fi
 
 packer build packer/alpine.json 
 
