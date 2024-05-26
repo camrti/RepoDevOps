be9a295730c967e311e8c2db320fc09ed6b1847b
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-26 15:52:45 +0200

    fix error thorwing

--- src/scopus/scopus.test.js
+++ src/scopus/scopus.test.js
@@ -7,28 +7,51 @@ const testCases = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'scopusTest
 describe('Testing scopus.js module', () => {
 
     describe('Testing getAuthorId function', () => {
-        test.each(testCases['scopus']['getAuthorId'])('search should return the correct authorID or null for %s', async ({ desc, name, surname, affiliation,  expectedResult, error}) => {
+        test.each(testCases['scopus']['getAuthorId'])('search should return the correct authorID or null for %s', async ({ desc, name, surname, affiliation,  expectedResult, errorFlag}) => {
             console.log("Test Case: ", desc);
-            if (!error){
-                const authorID = await scopus.getAuthorId(name, surname, affiliation)
-                expect(authorID).toEqual(expectedResult);
+            if (!errorFlag){
+                try {
+                    const authorID = await scopus.getAuthorId(name, surname, affiliation);
+                    expect(authorID).toEqual(expectedResult);
+                } catch (error) {
+                    // If an error occurs unexpectedly, fail the test
+                    fail(error);
+                }
             } else {
-                expect(() => scopus.getAuthorId(name, surname, affiliation)).toThrow();
+                try {
+                    await scopus.getAuthorId(name, surname, affiliation);
+                    // If no error occurs when error was expected, fail the test
+                    fail('Expected an error but did not get one');
+                } catch (error) {
+                    // If an error occurs as expected, pass the test
+                    expect(error).toBeDefined();
+                }
             }
             
         });
     })
 
     describe('Testing getAuthorDetails function', () => {
-        test.each(testCases['scopus']['getAuthorDetails'])('search should return the correct author detail or null for %s', async ({ desc, authorId,  expectedResult, error}) => {
+        test.each(testCases['scopus']['getAuthorDetails'])('search should return the correct author detail or null for %s', async ({ desc, authorId,  expectedResult, errorFlag}) => {
             console.log("Test Case: ", desc);
-            if (!error){
-                const authorDetails = await scopus.getAuthorDetails(authorId)
-                expect(authorDetails).toEqual(expectedResult);
+            if (!errorFlag){
+                try {
+                    const authorDetails = await scopus.getAuthorDetails(authorId);
+                    expect(authorDetails).toEqual(expectedResult);
+                } catch (error) {
+                    // If an error occurs unexpectedly, fail the test
+                    fail(error);
+                }
             } else {
-                expect(() => scopus.getAuthorDetails(authorId)).toThrow()
+                try {
+                    await scopus.getAuthorDetails(authorId);
+                    // If no error occurs when error was expected, fail the test
+                    fail('Expected an error but did not get one');
+                } catch (error) {
+                    // If an error occurs as expected, pass the test
+                    expect(error).toBeDefined();
+                }
             }
-            
         });
     })
     
