e8a2c1516b94077c93583a8dfbb040d5345bb4e4
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-26 12:43:55 +0200

    Try to test scopus.test.js

--- src/scopus/scopus.test.js
+++ src/scopus/scopus.test.js
@@ -4,7 +4,7 @@ const scopus = require('./scopus.js');
 
 const testCases = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'scopusTestCases.json'), 'utf8'));
 
-describe.skip('Testing scopus.js module', () => {
+describe('Testing scopus.js module', () => {
     test.each(testCases['scopus'])('search should return the correct authorID or null for %s', async ({ desc, name, surname, affiliation,  expectedResult}) => {
         console.log("Test Case: ", desc);
         const authorID = await scopus.getAuthorId(name, surname, affiliation)
