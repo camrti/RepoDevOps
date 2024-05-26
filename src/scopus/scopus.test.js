02f7ad99701d743c7586b708c263ccccad957f2b
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-05-26 16:54:24 +0200

    Fix

--- src/scopus/scopus.test.js
+++ src/scopus/scopus.test.js
@@ -24,7 +24,7 @@ describe('Testing scopus.js module', () => {
                     fail('Expected an error but did not get one');
                 } catch (error) {
                     // If an error occurs as expected, pass the test
-                    expect(error).toBeInstanceOf(TypeError);
+                    expect(error).toBeInstanceOf(Error);
                 }
             }
             
