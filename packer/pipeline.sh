5e9a710f214698d5ad5c7ca7fc60ba00d76f9996
Giorgio <g.galasso31@studenti.unisa.it>
2024-06-05 19:28:52 +0200

    update pipeline.sh

--- packer/pipeline.sh
+++ packer/pipeline.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Remove the temp file if it exists
-if [ -f /tmp/test_result.txt ]; then
-    rm /tmp/test_result.txt
-fi
-
 # Array of container names to stop
 containers=("my-search-container" "my-cineca-container" "my-scholar-container" "my-scopus-container")
 
@@ -41,17 +36,3 @@ packer build packer/packer_builds.json
 docker network create --driver=bridge --subnet=192.168.100.0/24 devops-net
 
 ansible-playbook packer/manage_containers_test.yml
-
-# Run Newman tests
-newman run postman/postman_test_project.postman_collection.json -d postman/researcher_test.json -r json --reporter-json-export postman/output_test.json
-
-ansible-playbook postman/newman_test_check.yaml
-
-if [ -f /tmp/test_result.txt ]; then
-    echo "Tests failed."
-    rm /tmp/test_result.txt
-    exit 1
-else
-    echo "All tests passed."
-    exit 0
-fi
