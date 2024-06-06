d3a25dd75826e2d2a852344b0088d2f677853db8
pax7898 <166528656+pcaggiano2@users.noreply.github.com>
2024-06-06 17:56:55 +0200

    Add post-receive hook and deployment.sh

---
+++ packer-ansible/deployment.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Base Path
+BASE_PATH="/home/devops/RepoDevOps"
+cd "${BASE_PATH}"
+
+# Go to branch develop and pull
+git checkout main
+git pull
+
+# Array of container names to stop
+containers=("my-search-container" "my-cineca-container" "my-scholar-container" "my-scopus-container")
+
+# Loop through the array and stop each container
+for container in "${containers[@]}"
+do
+    echo "Stopping container: $container"
+    docker stop "$container"
+done
+echo "All containers have been stopped."
+
+
+# Loop through the array and remove each container
+for container in "${containers[@]}"
+do
+    echo "Removing container: $container"
+    docker rm "$container"
+done
+echo "All containers have been removed."
+
+echo "Starting ansilble palybook..."
+ansible-playbook "packer-ansible/manage_containers_test.yml"
+
+if [[ $? -eq 0 ]]; then
+    echo "Ansible manage_container playbook for deployment completed successfully."
+else
+    echo "Ansible manage_container playbook for deployment failed. Aborting Deployment."
+    exit 1
+fi
+
+exit 0
